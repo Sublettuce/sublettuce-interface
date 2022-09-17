717a5f9a2200b8b8e853a07d6c98486e769bf291
Kai Morciniec <kaimorciniec@gmail.com>
2022-09-17 17:33:56 +0200

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
