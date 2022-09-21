350a63341b01e6c1d9873e244dfa6562f62d5eba
Kai Morciniec <kaimorciniec@gmail.com>
2022-09-21 05:15:28 +0200

    move card styles to seperate file

---
+++ src/components/ListDomainsOverview/DomainCard.styles.tsx
@@ -0,0 +1,29 @@
+import { createStyles } from "@mantine/core";
+
+export default createStyles((theme, _params, getRef) => {
+    const scaled: any = getRef("scaled");
+
+    return {
+        card: {
+            position: "relative",
+            backgroundColor:
+                theme.colorScheme === "dark"
+                    ? theme.colors.dark[5]
+                    : theme.white,
+            border: `1px solid ${
+                theme.colorScheme === "dark"
+                    ? theme.colors.dark[7]
+                    : theme.colors.gray[2]
+            }`,
+
+            [`&:hover .${scaled}`]: {
+                transform: "scale(1.04)",
+            },
+        },
+        scaled: {
+            ref: scaled,
+            flexGrow: 1,
+            transition: "transform 500ms ease",
+        },
+    };
+});
--- src/components/ListDomainsOverview/ListDomainCard.tsx
+++ src/components/ListDomainsOverview/ListDomainCard.tsx
@@ -1,39 +1,5 @@
-import {
-    Button,
-    Card,
-    Container,
-    createStyles,
-    Group,
-    Text,
-} from "@mantine/core";
-
-const useStyles = createStyles((theme, _params, getRef) => {
-    const scaled: any = getRef("scaled");
-
-    return {
-        card: {
-            position: "relative",
-            backgroundColor:
-                theme.colorScheme === "dark"
-                    ? theme.colors.dark[5]
-                    : theme.white,
-            border: `1px solid ${
-                theme.colorScheme === "dark"
-                    ? theme.colors.dark[7]
-                    : theme.colors.gray[2]
-            }`,
-
-            [`&:hover .${scaled}`]: {
-                transform: "scale(1.04)",
-            },
-        },
-        scaled: {
-            ref: scaled,
-            flexGrow: 1,
-            transition: "transform 500ms ease",
-        },
-    };
-});
+import { Button, Card, Container, Group, Text } from "@mantine/core";
+import useStyles from "./DomainCard.styles";
 
 export default function ListDomainCard({ domain }: { domain: any }) {
     const { classes } = useStyles();
