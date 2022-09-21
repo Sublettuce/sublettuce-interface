799312441a488f99e04debedab0d8f675b47264e
Kai Morciniec <kaimorciniec@gmail.com>
2022-09-21 02:44:57 +0200

    add listDomainCard grid

--- src/components/ListDomainsOverview/index.tsx
+++ src/components/ListDomainsOverview/index.tsx
@@ -1,5 +1,7 @@
+import { SimpleGrid } from "@mantine/core";
 import { useQuery } from "urql";
 import { useAccount } from "wagmi";
+import ListDomainCard from "./ListDomainCard";
 
 export default function ListDomainsOverview() {
     const { address } = useAccount();
@@ -26,6 +28,19 @@ export default function ListDomainsOverview() {
         return <div></div>;
     }
     const userDomains = result.data?.accounts[0];
-
-    return <div>{userDomains.wrappedDomains[0].domain.name}</div>;
+    console.log(userDomains.wrappedDomains);
+    return (
+        <SimpleGrid
+            cols={4}
+            breakpoints={[
+                { maxWidth: 1000, cols: 3 },
+                { maxWidth: 755, cols: 2 },
+                { maxWidth: 500, cols: 1 },
+            ]}
+        >
+            {userDomains.wrappedDomains?.map((domain: any) => (
+                <ListDomainCard key={domain.id} domain={domain.domain} />
+            ))}
+        </SimpleGrid>
+    );
 }
