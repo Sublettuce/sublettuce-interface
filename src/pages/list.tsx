a9eaf282b6a37699a1cae5a9a9ddc11b69bb2efc
Kai Morciniec <kaimorciniec@gmail.com>
2022-09-21 05:15:00 +0200

    add list page title and padding

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,9 +1,13 @@
+import { Title } from "@mantine/core";
 import type { NextPage } from "next";
 import ListDomainsOverview from "../components/ListDomainsOverview";
 
 const Home: NextPage = () => {
     return (
-        <div>
+        <div className="p-10">
+            <Title order={2} mb={10}>
+                Your domains
+            </Title>
             <ListDomainsOverview />
         </div>
     );
