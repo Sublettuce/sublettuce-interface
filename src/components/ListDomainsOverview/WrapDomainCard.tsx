a98b9f03cf7dbb28c3de97a77411bf89f3e8f866
Kai Morciniec <kaimorciniec@gmail.com>
2022-09-21 05:17:10 +0200

    add initial WrapDomainCard

---
+++ src/components/ListDomainsOverview/WrapDomainCard.tsx
@@ -0,0 +1,19 @@
+import { Button, Card, Container, Group, Text } from "@mantine/core";
+import useStyles from "./DomainCard.styles";
+
+export default function ListDomainCard({ domain }: { domain: any }) {
+    const { classes } = useStyles();
+    return (
+        <Card className={classes.card} shadow="sm" radius="md">
+            <Group>
+                <Container className={classes.scaled}>
+                    <Text size="lg">
+                        <b>{domain.name.slice(0, -4)}</b>
+                        {domain.name.slice(-4)}
+                    </Text>
+                </Container>
+                <Button>Wrap</Button>
+            </Group>
+        </Card>
+    );
+}
