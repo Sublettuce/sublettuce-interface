f1ad042599378e8f3e97a537ed01a791aca3e852
Kai Morciniec <kaimorciniec@gmail.com>
2022-09-25 01:16:50 +0100

    add basic domain info

--- src/pages/name/[name].tsx
+++ src/pages/name/[name].tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useQuery } from "urql";
-import { Title, LoadingOverlay } from "@mantine/core";
+import { Title, LoadingOverlay, List, ThemeIcon } from "@mantine/core";
+import { IconClipboardCheck, IconClock, IconSubtask } from "@tabler/icons";
+import dayjs from "dayjs";
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -11,6 +13,10 @@ const Home: NextPage = () => {
     {
         domains(where: {name: "${name}"}) {
             id
+            subdomainCount
+            owner {
+              id
+            }
             registration {
                 registrationDate
                 expiryDate
@@ -29,11 +35,51 @@ const Home: NextPage = () => {
   if (!result.data) {
     return <LoadingOverlay visible={true} />;
   }
+
+  const domain = result.data.domains[0];
+
+  if (!domain) {
+    return <div className="text-center">Unregistered domain</div>;
+  }
+  console.log(result.data);
   return (
-    <div className="p-10 text-center">
-      <Title order={1} mb={10}>
+    <div className="p-10 m-auto w-fit">
+      <Title mb={10} align="center">
         {name}
       </Title>
+      <List spacing="xs" size="sm">
+        <List.Item
+          icon={
+            <ThemeIcon color="teal" size={24} radius="xl">
+              <IconClipboardCheck size={16} />
+            </ThemeIcon>
+          }
+        >
+          Registered:{" "}
+          {dayjs
+            .unix(domain.registration.registrationDate)
+            .format("DD/MM/YYYY")}
+        </List.Item>
+        <List.Item
+          icon={
+            <ThemeIcon color="grey" size={24} radius="xl">
+              <IconClock size={16} />
+            </ThemeIcon>
+          }
+        >
+          Expires:{" "}
+          {dayjs.unix(domain.registration.expiryDate).format("DD/MM/YYYY")}
+        </List.Item>
+        <List.Item
+          icon={
+            <ThemeIcon color="blue" size={24} radius="xl">
+              <IconSubtask size={16} />
+            </ThemeIcon>
+          }
+        >
+          {domain.subdomainCount} registered subdomains
+        </List.Item>
+      </List>
     </div>
   );
 };
