1fa5626a6f8f5d000480f9ba5299107c0adb71a0
Kai Morciniec <kaimorciniec@gmail.com>
2022-09-25 01:54:11 +0100

    add listings/offers accordion

--- src/pages/name/[name].tsx
+++ src/pages/name/[name].tsx
@@ -1,13 +1,50 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useQuery } from "urql";
-import { Title, LoadingOverlay, List, ThemeIcon, Box } from "@mantine/core";
-import { IconClipboardCheck, IconClock, IconSubtask } from "@tabler/icons";
+import {
+  Title,
+  LoadingOverlay,
+  List,
+  ThemeIcon,
+  Box,
+  Group,
+  Accordion,
+  createStyles,
+} from "@mantine/core";
+import {
+  IconClipboardCheck,
+  IconClock,
+  IconSubtask,
+  IconTags,
+  IconList,
+} from "@tabler/icons";
 import dayjs from "dayjs";
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    paddingTop: theme.spacing.xl * 2,
+    paddingBottom: theme.spacing.xl * 2,
+    minHeight: 650,
+  },
+
+  title: {
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+
+  item: {
+    borderRadius: theme.radius.md,
+    marginBottom: theme.spacing.lg,
+
+    border: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+}));
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { name } = router.query;
+  const { classes } = useStyles();
 
   const QUERY_DOMAIN = `
     {
@@ -44,53 +81,80 @@ const Home: NextPage = () => {
   console.log(result.data);
   return (
     <div className="p-10 m-auto w-fit">
-      <Box
-        sx={(theme) => ({
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[6]
-              : theme.colors.gray[0],
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.lg,
-        })}
-      >
-        <Title mb={10} align="center">
-          {name}
-        </Title>
-        <List spacing="xs" size="sm">
-          <List.Item
-            icon={
-              <ThemeIcon color="teal" size={24} radius="xl">
-                <IconClipboardCheck size={16} />
-              </ThemeIcon>
-            }
-          >
-            Registered:{" "}
-            {dayjs
-              .unix(domain.registration.registrationDate)
-              .format("DD/MM/YYYY")}
-          </List.Item>
-          <List.Item
-            icon={
-              <ThemeIcon color="grey" size={24} radius="xl">
-                <IconClock size={16} />
-              </ThemeIcon>
-            }
-          >
-            Expires:{" "}
-            {dayjs.unix(domain.registration.expiryDate).format("DD/MM/YYYY")}
-          </List.Item>
-          <List.Item
-            icon={
-              <ThemeIcon color="blue" size={24} radius="xl">
-                <IconSubtask size={16} />
-              </ThemeIcon>
-            }
-          >
-            {domain.subdomainCount} registered subdomains
-          </List.Item>
-        </List>
-      </Box>
+      <Group spacing="xl" align="flex-start">
+        <Box
+          sx={(theme) => ({
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+            padding: theme.spacing.xl,
+            borderRadius: theme.radius.lg,
+            flexGrow: 1,
+          })}
+        >
+          <Title mb={10} align="center">
+            {name}
+          </Title>
+          <List spacing="xs" size="sm">
+            <List.Item
+              icon={
+                <ThemeIcon color="teal" size={24} radius="xl">
+                  <IconClipboardCheck size={16} />
+                </ThemeIcon>
+              }
+            >
+              Registered:{" "}
+              {dayjs
+                .unix(domain.registration.registrationDate)
+                .format("DD/MM/YYYY")}
+            </List.Item>
+            <List.Item
+              icon={
+                <ThemeIcon color="grey" size={24} radius="xl">
+                  <IconClock size={16} />
+                </ThemeIcon>
+              }
+            >
+              Expires:{" "}
+              {dayjs.unix(domain.registration.expiryDate).format("DD/MM/YYYY")}
+            </List.Item>
+            <List.Item
+              icon={
+                <ThemeIcon color="blue" size={24} radius="xl">
+                  <IconSubtask size={16} />
+                </ThemeIcon>
+              }
+            >
+              {domain.subdomainCount} registered subdomains
+            </List.Item>
+          </List>
+        </Box>
+        <Box className="flex-grow">
+          <Accordion multiple variant="separated">
+            <Accordion.Item className={classes.item} value="reset-password">
+              <Accordion.Control>
+                <Group align="flex-start">
+                  <IconTags />
+
+                  <Title size="sm">Subdomains listed by owner</Title>
+                </Group>
+              </Accordion.Control>
+              <Accordion.Panel>jifjeosfi</Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item className={classes.item} value="another-account">
+              <Accordion.Control>
+                <Group>
+                  <IconList />
+                  <Title size="sm">Offers</Title>
+                </Group>
+              </Accordion.Control>
+              <Accordion.Panel>heifeof</Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </Box>
+      </Group>
     </div>
   );
 };
